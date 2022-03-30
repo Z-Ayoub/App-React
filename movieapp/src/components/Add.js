eca625e45593980207f2725c9e927b6b8d6f25ab
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-30 22:16:59 +0200

    Update Add.js

--- movieapp/src/components/Add.js
+++ movieapp/src/components/Add.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import toto from './Add.module.css';
+import{useState} from 'react';
 const Add = () => {
+  const [query, setQuery] = useState("");
+
+  const onChange = (e) => {
+      e.preventDefault();
+
+      setQuery(e.target.value);
+  };
   return (
     <div className={toto.addpage}>
         <div className={toto.container}>
             <div className={toto.addContent}>
                 <div className={toto.inputWrapper}>
-                    <input type="text" placeholder="Search for a movie ..." />
+                    <input type="text" placeholder="Search for a movie ..." 
+                    value={query}
+                    onChange={onChange}
+                    />
                     
                 </div>
             </div>
