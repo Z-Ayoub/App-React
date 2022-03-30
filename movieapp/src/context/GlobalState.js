a4735baefbe5b92844d82335865d9859243b34d9
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-30 23:28:17 +0200

    update AppReducer.js & GlobalState.js

--- movieapp/src/context/GlobalState.js
+++ movieapp/src/context/GlobalState.js
@@ -14,8 +14,18 @@ export const GlobalContext = createContext(initialState);
 // Provider components
 export const GlobalProvider = props => {
     const [state, dispatch] = useReducer(AppReducer,initialState);
+    
+    // Actions
+    const addMovieToWatchlist = movies => {
+        dispatch({type: "ADD_MOVIE_TO_WATCHLIST", payload: movie});
+    }
+
     return (
-        <GlobalContext.Provider value={{watchlist: state.watchlist, watched: state.watched }}>
+        <GlobalContext.Provider 
+        value={{watchlist: state.watchlist,
+         watched: state.watched,
+          addMovieToWatchlist,
+          }}>
             {props.children}
         </GlobalContext.Provider>
     )
