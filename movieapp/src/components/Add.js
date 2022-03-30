b44c612b94b0919b53d57640bb3824253ae1f948
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-30 22:28:58 +0200

    Update Add.js

--- movieapp/src/components/Add.js
+++ movieapp/src/components/Add.js
@@ -8,6 +8,12 @@ const Add = () => {
       e.preventDefault();
 
       setQuery(e.target.value);
+
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      ).then((res) => res.json())
+      .then((data) => {
+          console.log(data);
+      });
   };
   return (
     <div className={toto.addpage}>
