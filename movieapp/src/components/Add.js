bb6adb2e8976413ae27e68cf58e0c2f1901f8761
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-30 22:34:53 +0200

    Update Add.js

--- movieapp/src/components/Add.js
+++ movieapp/src/components/Add.js
@@ -3,7 +3,7 @@ import toto from './Add.module.css';
 import{useState} from 'react';
 const Add = () => {
   const [query, setQuery] = useState("");
-
+  const [results, setResults] = useState([]);
   const onChange = (e) => {
       e.preventDefault();
 
@@ -12,7 +12,11 @@ const Add = () => {
       fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
       ).then((res) => res.json())
       .then((data) => {
-          console.log(data);
+          if(!data.errors){
+            setResults(data.results);
+          }else {
+            setResults([]);
+          }
       });
   };
   return (
@@ -24,8 +28,17 @@ const Add = () => {
                     value={query}
                     onChange={onChange}
                     />
-                    
                 </div>
+
+                {results.length > 0 && (
+                    <ul className="results">
+                        {results.map(movie => (
+                            <li>
+                                {movie.title}
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
     </div> 
