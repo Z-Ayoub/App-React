6451cff946be4581a0cdf1691998f78616a2d3c9
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-31 16:34:04 +0200

    Update MovieControls.js

--- movieapp/src/components/MovieControls.js
+++ movieapp/src/components/MovieControls.js
@@ -1,21 +1,23 @@
  import React from 'react';
  import toto from './MovieControls.module.css';
  import '../lib/font-awesome/css/all.min.css';
+ import { useContext  } from 'react';
+ import { GlobalContext } from '../context/GlobalState';
  
  const MovieControls = () => {
+   const {removeMovieFromWatchlist} = useContext(GlobalContext);     
    return (
      <div className={toto.innerCardControls}>
          { type === "watchlist" && (
              <>
                 <Button className={toto.ctrlbtn}>
-                    <i className="fa-fw far fa-eye">
-
-                    </i>
-                    <i className="fa-fw fa fa-times">
-
-                    </i>
+                    <i className="fa-fw far fa-eye"></i>
                 </Button>
+                <Button className={toto.ctrlbtn} 
+                onClick={() => removeMovieFromWatchlist(movie.id)}>
 
+                    <i className="fa-fw fa fa-times"></i>
+                </Button>
              </>
          )}
      </div>
