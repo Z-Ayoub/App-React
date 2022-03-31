d3cdddab726ddb78426acf770b7a43c111c92765
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-31 13:53:59 +0200

    update Add.js & index.js

--- movieapp/src/components/Add.js
+++ movieapp/src/components/Add.js
@@ -2,47 +2,34 @@ import React from 'react';
 import toto from './Add.module.css';
 import {useState} from 'react';
 import ResultCard from './ResultCard';
+
 const Add = () => {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
+  const [query,setQuery] = useState("");
+
   const onChange = (e) => {
       e.preventDefault();
 
       setQuery(e.target.value);
-
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      fetch(`https://api.themoviedb.org/3/search/movie?api_key=6453b53f4533c3e763c494b766ff3cf3&language=en-US&page=1&include_adult=false&query=${e.target.value}`
       ).then((res) => res.json())
       .then((data) => {
-          if(!data.errors){
-            setResults(data.results);
-          }else {
-            setResults([]);
-          }
+          console.log(data);
       });
-  };
+  }
   return (
-    <div className={toto.addpage}>
-        <div className={toto.container}>
-            <div className={toto.addContent}>
-                <div className={toto.inputWrapper}>
-                    <input type="text" placeholder="Search for a movie ..." 
-                    value={query}
-                    onChange={onChange}
-                    />
-                </div>
-
-                {results.length > 0 && (
-                    <ul className={toto.results}>
-                        {results.map(movie => (
-                            <li key={movie.id}>
-                                <ResultCard movie={movie} />
-                            </li>
-                        ))}
-                    </ul>
-                )}
-            </div>
+    <div className={toto.addPage}>
+      <div className={toto.container}>
+        <div className={toto.addContent}>
+          <div className={toto.inputWrapper}>
+            <input type="text" 
+            placeholder="Search for a movie"
+            value={query}
+            onChange={onChange}
+            />
+          </div>
         </div>
-    </div> 
+      </div>
+    </div>
   );
-}
+};
 export default Add;
