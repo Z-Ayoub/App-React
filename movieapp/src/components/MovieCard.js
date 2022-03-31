9a27daad625b76e6ad53eae17f4b5591b7ce539c
Z-Ayoub <ayoub.zinou@ecole-isitech.fr>
2022-03-31 15:07:24 +0200

    Update

---
+++ movieapp/src/components/MovieCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import toto from './MovieCard.module.css';
+const MovieCard = ({movie,type}) => {
+    return(
+        <div className={toto.movieCard}>
+            <div className={toto.overlay}>
+
+            </div>
+            {movie.poster_path ? (
+                <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} 
+                alt={`${movie.title} Poster`} />
+            ): (
+                <div className={toto.fillerPoster}></div>
+            )}
+        </div>
+    );
+}
+export default MovieCard;
--- movieapp/src/components/Watchlist.js
+++ movieapp/src/components/Watchlist.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
-
+import MovieCard from './MovieCard';
+import toto from './Watchlist.module.css';
 const Watchlist = () => {
     const {watchlist} = useContext(GlobalContext);
     return (
-        <div>
-            {watchlist.map((movie) => (
-                <h1>{movie.title}</h1>
-            ))}
+        <div className={toto.movieApp}>
+            <div className={toto.container}>
+                <div className={toto.header}>
+                    <h1 className={toto.heading}>My Watchlist</h1>
+                </div>
+
+                <div className={toto.movieGrid}>
+                   {watchlist.map(movie => (
+                       <MovieCard movie={movie} type="watchlist"/>
+                    ))}
+                </div>
+
+            </div>
         </div>
     );
 }
